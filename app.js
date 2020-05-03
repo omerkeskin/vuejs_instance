120b4b77ea422d582c4bcdac9d5ce1f0bfbc2e55
Ömer Keskin <omer.keskin@softtech.com.tr>
2020-05-03 16:37:39 +0300

    Second vue instance

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
-new Vue({
-  el: '#app',
+var vm1 = new Vue({
+  el: '#app1',
   data: {
     title: 'VueJS Instance',
     showParagraph: false
@@ -21,6 +21,19 @@ new Vue({
   watch: {
     title: function(value) {
       alert('Title değişti, yeni değer : ' + value);
+    }
+  }
+});
+
+var vm2 = new Vue({
+  el: '#app2',
+  data: {
+    title: 'VueJS Instance 2',
+    showParagraph: false
+  },
+  methods : {
+    changeTitle : function () {
+       vm1.title = 'Instance 2 tarafından değiştirildi.';
     }
   }
 });
